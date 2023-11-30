6929b7a4e726bb5b122e95e923188135fcf9ee87
CYECHARLA <142927564+CYECHARLA@users.noreply.github.com>
2023-11-29 23:29:33 -0600

    added secure

--- routes/cars.js
+++ routes/cars.js
@@ -1,7 +1,20 @@
 var express = require("express");
 const car_controlers = require("../controllers/CarController");
 var router = express.Router();
-// GET carss
+
+// Sample middleware for basic authentication
+const authenticate = (req, res, next) => {
+  // You can implement your authentication logic here
+  // For example, check if the user is logged in or has the necessary permissions
+  const isLoggedIn = /* Logic to check if user is logged in */;
+  if (isLoggedIn) {
+    next(); // Continue to the next middleware/route handler if authenticated
+  } else {
+    res.status(401).send("Unauthorized"); // Unauthorized access
+  }
+};
+
+// GET cars
 router.get("/", car_controlers.car_view_all_Page);
 
 // GET request for one car.
@@ -13,14 +26,13 @@ router.put('/cars/:id', car_controlers.car_update_put);
 /* GET detail car page */
 router.get('/detail', car_controlers.car_view_one_Page);
 
-/* GET create car page */
-router.get('/create', car_controlers.car_create_Page);
+/* GET create car page - Protected with authentication middleware */
+router.get('/create', authenticate, car_controlers.car_create_Page);
 
-/* GET create update page */
+/* GET update car page */
 router.get('/update', car_controlers.car_update_Page);
 
-/* GET delete car page */
-router.get('/delete', car_controlers.car_delete_Page);
+/* GET delete car page - Protected with authentication middleware */
+router.get('/delete', authenticate, car_controlers.car_delete_Page);
 
 module.exports = router;
-
