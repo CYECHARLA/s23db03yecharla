6de9b3fd1485ee5fa117835acf51a73f0a1d0e3f
CYECHARLA <142927564+CYECHARLA@users.noreply.github.com>
2023-11-29 23:15:11 -0600

    added new pugs file

--- app.js
+++ app.js
@@ -10,124 +10,91 @@ var LocalStrategy = require('passport-local').Strategy;
 const mongoose = require("mongoose");
 
 var resourceRouter = require('./routes/resource');
-
-var carsRouter = require('./routes/cars');
-
+var carsRouter = require('./routes/cars');  // Fixed variable name
 
 require('dotenv').config();
 
-const connectionString = process.env.MONGO_CON
+const connectionString = process.env.MONGO_CON;
 
 mongoose.connect(connectionString, {
-
   useNewUrlParser: true,
-
   useUnifiedTopology: true,
-
 })
-
-  .then(() => console.log("Connected to MongoDB"))
-
-  .catch((err) => console.log("Error Connecting to MongoDB: ", err));
-
-//Get the default connection
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.log("Error Connecting to MongoDB: ", err));
 
 var db = mongoose.connection;
 
-//Bind connection to error event
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-db.once("open", function () { console.log("Connection to DB succeeded") })
+db.once("open", function() { console.log("Connection to DB succeeded"); });
 
 passport.use(new LocalStrategy(
-  function (username, password, done) {
-    Account.findOne({ username: username })
-      .then(function (user) {
-        if (err) { return done(err); }
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!user.validPassword(password)) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-      })
-      .catch(function (err) {
-        return done(err)
-      })
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
   })
-)
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var carsRouter = require('./routes/cars');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
+
 var Car = require("./models/car");
 
+var Account =require('./models/account');
 
 async function recreateDB() {
-
-  // Delete everything
-
   await Car.deleteMany();
-
-  let instance1 = new Car(
-
-    {
-
-      Make: "Toyota",
+ 
+  let instance1 = new Car({
+    Make: "Toyota",
 
       Model: 'Camry',
 
       Price: 25000
+  });
 
-    });
+  let instance2 = new Car({
+    Make: "Honda",
 
-  let instance2 = new Car(
+    Model: 'Civic',
 
-    {
+    Price: 22000
+  });
 
-
-      Make: "Honda",
-
-      Model: 'Civic',
-
-      Price: 22000
-
-    });
-
-  let instance3 = new Car(
-
-    {
-
-      Make: "Ford",
+  let instance3 = new Car({
+    Make: "Ford",
 
       Model: 'F-150',
 
       Price: 3500
-
-    });
+  });
 
   instance1.save()
-
-    .then(doc => { console.log("First object saved") })
-
-    .catch(err => { console.error(err) })
+  .then(doc => { console.log("First object saved"); })
+  .catch(err => { console.error(err); });
 
   instance2.save()
-
-    .then(doc => { console.log("Second object saved") })
-
-    .catch(err => { console.error(err) })
+  .then(doc => { console.log("Second object saved"); })
+  .catch(err => { console.error(err); });
 
   instance3.save()
-
-    .then(doc => { console.log("Third object saved") })
-
-    .catch(err => { console.error(err) })
-
+  .then(doc => { console.log("Third object saved"); })
+  .catch(err => { console.error(err); });
 }
 
 let reseed = true;
@@ -136,15 +103,22 @@ if (reseed) { recreateDB(); }
 
 var app = express();
 
-// view engine setup
-app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -153,31 +127,26 @@ app.use('/car', carsRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 
+app.use('/cars', carsRouter);  // Updated variable name
 app.use("/resource", resourceRouter);
-app.use('/cars', carsRouter);
 
-app.use(require('express-session')({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
+// passport config
+// Use the existing connection
+// The Account model
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser())
 
 module.exports = app;
